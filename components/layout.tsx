4d1de798e29f54124cf56553769f7e26490b7a8c
GitHub Action <action@github.com>
2022-10-16 15:27:52 +0000

    Generated mats-first-react-app from Cookiecutter

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '{{ cookiecutter.repo_name }}'
-export const siteTitle = '{{ cookiecutter.repo_name }} Next.js-React Sample Website'
+const name = 'mats-first-react-app'
+export const siteTitle = 'mats-first-react-app Next.js-React Sample Website'
 
 export default function Layout({
   children,
